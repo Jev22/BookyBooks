0c7f172a3dcc90a2ef5c9dcbabc6a7ab0f4c12e8
Jev22 <50078304+Jev22@users.noreply.github.com>
2022-05-18 22:26:58 +0200

    Added Display of categories, bootstrap stying, razerpage compilation and some new styling

--- BookWeb/Controllers/CategoryController.cs
+++ BookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BookWeb.Data;
+using BookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookWeb.Controllers
@@ -13,8 +14,11 @@ namespace BookWeb.Controllers
         }
         public IActionResult Index()
         {
-            var objCategoryList = _db.Category.ToList();
-            return View();
+            // var objCategoryList = _db.Category.ToList();
+            // I stedet for at bruge var, så foreslåes det at bruge IEnumerable<> da det er strongly typed
+            IEnumerable<Category> objCategoryList = _db.Category.ToList(); //Da vi bruger IEnumerable, vil dette stadig virke uden .ToList();
+            //IEnumerable<Category> vil også være vores "model" som vi kalder i category/index.cshtml
+            return View(objCategoryList);
         }
     }
 }
